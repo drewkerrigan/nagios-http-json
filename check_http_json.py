b2cedec546b042a9aae4b6e846a9968816acfe4c
Markus Opolka <markus.opolka@netways.de>
2026-04-28 15:59:00 +0200

    Bump to v2.4.0

--- check_http_json.py
+++ check_http_json.py
@@ -25,8 +25,8 @@ WARNING_CODE = 1
 CRITICAL_CODE = 2
 UNKNOWN_CODE = 3
 
-__version__ = '2.3.1'
-__version_date__ = '2026-02-25'
+__version__ = '2.4.0'
+__version_date__ = '2026-05-04'
 
 class NagiosHelper:
     """
